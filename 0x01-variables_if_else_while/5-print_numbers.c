9a028702a86f98cbd2b4a0b14be41bfd270675c6
Otekaye Seun <otekayeseun24@gmail.com>
2023-09-08 08:57:03 -0700

    "5-print_numbers"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints out single digits number of base ten
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
