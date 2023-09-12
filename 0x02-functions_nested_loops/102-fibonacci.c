1c34b882422e45002d1e2a0d240f6aafa1d4fc76
Otekaye Seun <otekayeseun24@gmail.com>
2023-09-12 12:35:13 -0700

    "102-fibonacci"

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	long b = 1, c = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%ld", b);
+		else if (a == 1)
+			printf(", %ld", c);
+		else
+		{
+			c += b;
+			b = c - b;
+			printf(", %ld", c);
+		}
+		++a;
+	}
+	printf("\n");
+	return (0);
+}
