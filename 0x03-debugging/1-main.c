7ed2a691a50c986bdfc5fa0e439f66b098921cd2
Otekaye Seun <otekayeseun24@gmail.com>
2023-09-14 04:00:25 -0700

    "1-main"

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,15 @@ int main(void)
 {
 	int i;
 
-	printf("Infinte loop incoming:(\n");
+	printf("Infinte loop incoming :(\n");
+
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-	printf("Infinite loop avoided!\\o/\n");
+
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
