3039a2d4b6aee0c239e39d22c593e40af76c4bab
Otekaye Seun <otekayeseun24@gmail.com>
2023-09-18 16:59:07 -0700

    "6-puts2"

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: String printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int x;
+	int y = 0;
+
+	while (str[y] != '\0')
+	{
+		y++;
+	}
+
+	for (x = 0; x < y; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
