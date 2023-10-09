49e0d09436c2fc36f8bb47f6349acd6b6883be39
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-08 17:58:52 -0700

    "1-string_nconcat"

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		l = x + n;
 
-	s = malloc(sizeof(char) * 1 + 1);
+	s = malloc(sizeof(char) * l + 1);
 
 	if (s == NULL)
 		return (NULL);
