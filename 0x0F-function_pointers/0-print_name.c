cfb53175b55413a6e605e82e6f86478e0157c72e
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-11 20:19:41 -0700

    "0-print_name"

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: Name to be printed
+ * @f: pointer to function
+ * Return: Printed name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
