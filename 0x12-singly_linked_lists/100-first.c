975084a10544b6c8c2c9c3b3f5ac0e5a48ec4d94
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-19 18:25:09 -0700

    "100-first"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	printf("You're the beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n);
+	printf("I bore my house upon my back!\n");
 }
