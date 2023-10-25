cccb57cecc5d1b870caecbbe3a283b065bafc97e
Otekaye Seun <otekayeseun24@gmail.com>
2023-10-25 16:05:42 -0700

    "101-print_listint_safe"

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,88 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
-/**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
- */
-
-size_t looped_listint_len(const listint_t *head)
-{
-	const listint_t *mole, *len;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	mole = head->next;
-	len = (head->next)->next;
-
-	while (len)
-	{
-		if (mole == len)
-		{
-			mole = head;
-			while (mole != len)
-			{
-				nodes++;
-				mole = mole->next;
-				len = len->next;
-			}
-
-			mole = mole->next;
-			while (mole != len)
-			{
-				nodes++;
-				mole = mole->next;
-			}
-
-			return (nodes);
-		}
-
-		mole = mole->next;
-		len = (len->next)->next;
-	}
-
-	return (0);
-}
-
 /**
  * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * @head: head of the linked list
  * Return: The number of nodes in the list.
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, love = 0;
+	const listint_t *love = head, *hate = head;
+	size_t mend = 0;
+	int un_loop = 0;
 
-	nodes = looped_listint_len(head);
-
-	if (nodes == 0)
+	while (love && hate && hate->next)
 	{
-		for (; head != NULL; nodes++)
+		if (!(hate->next->next))
+			break;
+		love = love->next;
+		hate = hate->next->next;
+		if (love == hate)
 		{
+			love = love->next;
+			un_loop = 1;
+			break;
+		}
+	}
+	if (!un_loop)
+	{
+		while (head)
+		{
+			mend++;
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
+		return (mend);
 	}
-
-	else
+	while (head)
 	{
-		for (love = 0; love < nodes; love++)
+		mend++;
+		if (head == love)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			exit(98);
 		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
-	return (nodes);
+	return (0);
 }
