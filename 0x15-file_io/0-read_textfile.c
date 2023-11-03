6c832859582200a31a84379fa9b20cc0144f23b2
Otekaye Seun <otekayeseun24@gmail.com>
2023-11-02 21:57:01 -0700

    "0-read_textfile"

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: filename (pointer of string)
+ * @letters: numbers of letters printed
+ * Return: numbers of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nread, nwrite;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
+		return (0);
+
+	nread = read(fd, buff, letters);
+	nwrite = write(STDOUT_FILENO, buff, nread);
+
+	close(fd);
+	free(buff);
+
+	return (nwrite);
+}
