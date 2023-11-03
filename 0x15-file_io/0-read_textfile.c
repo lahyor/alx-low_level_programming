9006b779cc992b33ba4d3b90790f5ff578c181d1
Otekaye Seun <otekayeseun24@gmail.com>
2023-11-02 22:08:27 -0700

    "0-read_textfile"

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,12 +16,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char *) * letters);
+	buff = malloc(sizeof(char) * letters);
 
 	if (buff == NULL)
 		return (0);
@@ -32,5 +32,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buff);
 	close(fd);
 
-	return (nread);
+	return (nwrite);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _FILE_IO__
-#define _FILE_IO__
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
 #include <unistd.h>
 #include <stdlib.h>
