6a96722751c7472d9b3ced1fdf72053b443c6bd4
Otekaye Seun <otekayeseun24@gmail.com>
2023-12-22 14:01:49 -0800

    "When will it end"

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
+	unsigned long int c;
+
+	for (c = 0; c < ht->size; c++)
+	{
+		if (ht->array[c] != NULL)
+		{
+			node = ht->array[c];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
