5f21ef60badfd60eaac6f8de1c1e7a586b05c047
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-11 17:17:35 -0700

    "Need"

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -4,6 +4,7 @@
 /**
  * jump_list - searches for a value in an array of
  * integers using the Jump search algorithm
+ *
  * @list: input list
  * @size: size of the array
  * @value: value to search in
@@ -11,37 +12,37 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t ind, l, m;
+	size_t index, k, m;
 	listint_t *prev;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
 	m = (size_t)sqrt((double)size);
-	ind = 0;
-	l = 0;
+	index = 0;
+	k = 0;
 
 	do {
 		prev = list;
-		l++;
-		ind = l * m;
+		k++;
+		index = k * m;
 
-		while (list->next && list->ind < ind)
+		while (list->next && list->index < index)
 			list = list->next;
 
-		if (list->next == NULL && ind != list->ind)
-			ind = list->ind;
+		if (list->next == NULL && index != list->index)
+			index = list->index;
 
-		printf("Value checked at index [%d] = [%d]\n", (int)ind, list->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
 
-	} while (ind < size && list->next && list->n < value);
+	} while (index < size && list->next && list->n < value);
 
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)prev->ind, (int)list->ind);
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
 
-	for (; prev && prev->ind <= list->ind; prev = prev->next)
+	for (; prev && prev->index <= list->index; prev = prev->next)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)prev->ind, prev->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
 		if (prev->n == value)
 			return (prev);
 	}
